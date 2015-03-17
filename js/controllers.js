a134de3acaefc907b6e27db81eb55ec4c8f4a475
jagruti <patiljagruti181@gmail.com>
2015-03-17 16:43:57 +0530

    ref close
    ref close

--- js/controllers.js
+++ js/controllers.js
@@ -447,7 +447,7 @@ angular.module('starter.controllers', ['myservices', 'base64', 'ionic.rating'])
 
         } else {
             if (data.Data[0].TransactionMessage == "Transaction Successful" && data.Data[0].TransactionStatus == "SUCCESS") {
-                //ref.close();
+                ref.close();
                 $interval.cancel(stopinterval);
                 $location.url("/app/home/details/quotes/book/summary/thankyou")
                 //				var alertPopup = $ionicPopup.alert({
@@ -455,7 +455,7 @@ angular.module('starter.controllers', ['myservices', 'base64', 'ionic.rating'])
                 //					template: 'Transaction Successful'
                 //				});
             } else {
-                //ref.close();
+                ref.close();
                 $interval.cancel(stopinterval);
                 var alertPopup = $ionicPopup.alert({
                     title: 'Transaction Error',
